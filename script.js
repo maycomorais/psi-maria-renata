7f81eb7e6e78b87f56abc45fede4c7ec7d38d40f
Maycon Morais <maycomorais@gmail.com>
2022-10-20 22:55:22 -0300

    feature new sections

--- script.js
+++ script.js
@@ -49,18 +49,6 @@ $(document).ready(function(){
     });
 
 
-    var typed = new Typed(".typing", {
-        strings: ["Psicóloga"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
-    var typed = new Typed(".typing-2", {
-        strings: ["Psicóloga"],
-        typeSpeed: 100,
-        backSpeed: 60,
-        loop: true
-    });
     
 });
 
